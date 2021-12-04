ee06549ebec7c281bcb8f16d7cb3ac0a4cb8041b
KyleCaulkins <90616094+KyleCaulkins@users.noreply.github.com>
2021-12-04 08:46:16 -0600

    module content

--- plots.js
+++ plots.js
@@ -1,38 +1,48 @@
-Plotly.newPlot("plotArea", [{x: [1, 2, 3], y: [10, 20, 30]}]);
+//Don't forget, when running this in a browser you need to navigate to the folder in the command prompt and type "python -m http.server" to make a local server host
+//Put "localhost:8000" into browser to use the local server
+//There are securities in place to prevent the call to the .json file, actions above must be taken
 
-//Creating arrays of city names and growth figures
-var topFiveCityNames = cityGrowths.map(city => city.City);
-var topFiveCityGrowths = cityGrowths.map(city => parseInt(city.Increase_from_2016));
+//Module 12.4.3 making a drop down menu for the belly button data
+//Dynamically generate dropdown menu items
+function init() {
+    var selector = d3.select("#selDataset");
+  
+    d3.json("samples.json").then((data) => {
+      console.log(data);
+      var sampleNames = data.names;
+      sampleNames.forEach((sample) => {
+        selector
+          .append("option")
+          .text(sample)
+          .property("value", sample);
+      });
+  })}
+  
+  init();
 
-//Creating a bar chart with the arrays from above
-var trace = {
-    x: topFiveCityNames,
-    y: topFiveCityGrowths,
-    type: "bar"
-  };
-  var data = [trace];
-  var layout = {
-    title: "Most Rapidly Growing Cities",
-    xaxis: {title: "City" },
-    yaxis: {title: "Population Growth, 2016-2017"}
-  };
-  Plotly.newPlot("bar-plot", data, layout);
+//Function called when a change takes place in the dropdown menu
+function optionChanged(newSample) {
+    console.log(newSample);
+  }
 
-//Skill Drill cerat a bar chart of the seven largest cities
-//Create array
-var sevenLargestCityNames=cityGrowths.map(name =>name.City);
-var sevenLargestCityPopulations=city.Growths.map(cities=>parseInt(cities.population));
+//When ID is selected functions will be called
+function optionChanged(newSample) {
+    buildMetadata(newSample);
+    //buildCharts(newSample);
+  }
 
-//Create bar chart with largest cities array
-var trace = {
-    x: sevenLargestCityNames,
-    y: sevenLargestCityPopulations,
-    type: "bar"
-  };
-  var data = [trace];
-  var layout = {
-    title: "Seven Largest Cities",
-    xaxis: {title: "City" },
-    yaxis: {title: "Population"}
-  };
-  Plotly.newPlot("bar-plot", data, layout);
+//Building the "buildMetadata" fucntion to display demographic info
+//Skill drill, I can't help but feel left out on a limb for this one. No thank you...
+function buildMetadata(sample) {
+    d3.json("samples.json").then((data) => {
+      var metadata = data.metadata;
+      var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
+      var result = resultArray[0];
+      var PANEL = d3.select("#sample-metadata");
+      PANEL.html("");
+      Object.entries(result).forEach(([key, value])=>
+        {PANEL.append("h6").text(key + ': '+value)});
+    });
+  }    
+//Take that! Got it...        
+      
